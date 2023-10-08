4b576cb509640fb5c29d39e57eb5f78c90576ba9
Fron4i <ilya10306@mail.ru>
2023-10-08 12:25:39 +0300

    working slider on pop-up

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ const mobileNabButton = document.querySelector('.mobile-nav-button')
 const mobileNabIcon = document.querySelector('.mobile-nav-button__icon')
 const mobileNab = document.querySelector('.mobile-nav')
 
-
 mobileNabButton.addEventListener('click', $(function() {
 		$('#nav-icon3').click(function(){
 			$(this).toggleClass('open');
@@ -11,3 +10,53 @@ mobileNabButton.addEventListener('click', $(function() {
 			document.body.classList.toggle('no-scroll')
 		});
 	}))
+
+
+
+
+var elem = document.querySelector('.hero-slider');
+var flkty = new Flickity( elem, {
+	wrapAround: true,
+	autoPlay: 1850,
+	pauseAutoPlayOnHover: false,
+	prevNextButtons: false
+});
+
+var playButton1 = document.querySelector('.popup__close');
+var playButton2 = document.querySelector('.popup__area');
+var stopButton = document.querySelector('.btn-bell');
+
+$(".btn-bell").on("click", function() {
+		setTimeout(function() {
+			flkty.stopPlayer();
+		}, 100)
+})
+
+$(".popup__area").on("click", function() {
+		setTimeout(function() {
+			flkty.playPlayer();
+		}, 100)
+})
+
+function clisk() {
+	stopButton.classList.toggle('active')
+	flkty.playPlayer();
+
+	playButton2.addEventListener( 'click', function() {
+		flkty.playPlayer();
+	});
+
+	playButton1.addEventListener( 'click', function() {
+		flkty.playPlayer();
+	});
+}
+
+flkty.on( 'staticClick', function( event, pointer, cellElement, cellIndex ) {
+	flkty.playPlayer();
+	setTimeout(clisk, 100);
+});
+
+flkty.on( 'dragEnd', function( event, pointer ) { 
+	flkty.playPlayer();
+	setTimeout(clisk, 100);
+});
