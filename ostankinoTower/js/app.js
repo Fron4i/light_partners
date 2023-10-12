5b5017dddf67b76caee399c06b9816f3de2aef37
Fron4i <ilya10306@mail.ru>
2023-10-13 00:27:44 +0300

    ostankinoTower 40% and fix home slider height

--- ostankinoTower/js/app.js
+++ ostankinoTower/js/app.js
@@ -3,84 +3,72 @@ const mobileNabButton = document.querySelector('.mobile-nav-button')
 const mobileNabIcon = document.querySelector('.mobile-nav-button__icon')
 const mobileNab = document.querySelector('.mobile-nav')
 const headerLogo = document.querySelector('.header-logo')
+const footer = document.querySelector('.footer')
 const main = document.querySelector('.main')
+const click1 = document.querySelector('.click')
+const mapMenu = document.querySelector('.map-menu')
+const closeMenu = document.querySelector('.close-menu')
+
+
 
 const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth+ 'px'
-let widthHeader = document.querySelector('.header').offsetHeight + 'px';
+let widthHeader = document.querySelector('.header').offsetHeight + 5  + 'px';
 
 window.onload = () => {
 	main.style.paddingTop = widthHeader;
 	if (document.documentElement.clientWidth < 1080) headerLogo.style.paddingLeft = lockPaddingValue;
 }
+
+$(document).on("click.click", ".click", function(){
+	mapMenu.style.zIndex = '101';
+});
+
+$(document).on("click.close-menu", ".close-menu", function(){
+	mapMenu.style.zIndex = '-1';
+});
+
+// click1.addEventListener('click', $(function() {
+// 	$('.click').click(function(){
+// 		mapMenu.style.zIndex = '101';
+// 	});
+// }))
+
+// closeMenu.addEventListener('click', $(function() {
+// 	mapMenu.style.zIndex = '-1';
+// }))
+
 mobileNabButton.addEventListener('click', $(function() {
-		$('#nav-icon3').click(function(){
+	$('#nav-icon3').click(function(){
 
-			if (lockPadding.length > 0) {
-				for (let index = 0; index < lockPadding.length; index++) {
-					const el = lockPadding[index]
-					el.style.paddingRight = lockPaddingValue
-				}
+		if (lockPadding.length > 0) {
+			for (let index = 0; index < lockPadding.length; index++) {
+				const el = lockPadding[index]
+				el.style.paddingRight = lockPaddingValue
 			}
-			//! .header-logo
-			headerLogo.style.paddingRight = '0px'
-			body.style.paddingRight = lockPaddingValue
+		}
+		//! .header-logo
+		headerLogo.style.paddingRight = '0px'
+		main.style.paddingRight = lockPaddingValue
+		footer.style.paddingRight = lockPaddingValue
 
-			$(this).toggleClass('open');
-			mobileNabIcon.classList.toggle('active')
-			mobileNab.classList.toggle('active')
-			document.body.classList.toggle('no-scroll')
+		$(this).toggleClass('open');
+		mobileNabIcon.classList.toggle('active')
+		mobileNab.classList.toggle('active')
+		document.body.classList.toggle('no-scroll')
 
-			if (!mobileNabIcon.classList.contains('active')) {
-				
-					if (lockPadding.length > 0) {
-						for (let index = 0; index < lockPadding.length; index++) {
-							const el = lockPadding[index];
-							el.style.paddingRight = '0px'
-						}
+		if (!mobileNabIcon.classList.contains('active')) {
+				if (lockPadding.length > 0) {
+					for (let index = 0; index < lockPadding.length; index++) {
+						const el = lockPadding[index];
+						el.style.paddingRight = '0px'
 					}
-					//! .header-logo
-					body.style.paddingRight = '0px'
-			}
-		});
-	}))
-
-
-// ! Slider
-
-var elem = document.querySelector('.hero-slider');
-var flkty = new Flickity( elem, {
-	wrapAround: true,
-	autoPlay: 2200000,
-	pauseAutoPlayOnHover: false,
-	prevNextButtons: false
-});
-
-var playButton1 = document.querySelector('.close-popup');
-var stopButton = document.querySelector('.popup-link');
-
-$(".popup-link").on("click", function() {
-		setTimeout(function() {
-			flkty.stopPlayer();
-		}, 10)
-})
-
-function clisk() {
-	flkty.playPlayer();
-
-	playButton1.addEventListener( 'click', function() {
-		flkty.playPlayer();
+				}
+				//! .header-logo
+				main.style.paddingRight = '0px'
+				footer.style.paddingRight = '0px'
+		}
 	});
-}
-
-flkty.on( 'staticClick', function( event, pointer, cellElement, cellIndex ) {
-	flkty.playPlayer();
-	setTimeout(clisk, 10);
-});
-
-flkty.on( 'dragEnd', function( event, pointer ) { 
-	flkty.playPlayer();
-	setTimeout(clisk, 10);
-});
+}))
 
 // ? Popup
 
